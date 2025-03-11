1bc01fa0aeb7047d649b9a10c56da8e56db4c25f
Rafael Perez <rafaelperez@MacBook-Pro-2.local>
2025-03-11 16:15:34 -0400

    Add mongoose dependency and update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Prediction Schema (Updated with Payment Method)
+// Prediction Schema
 const predictionSchema = new mongoose.Schema({
   bracketPicks: [String],
   seriesLengths: [Number],
